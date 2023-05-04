92d598ae91feaf295cc4b3b2f14a9eeef7e15565
jcduff00 <johncduffman@gmail.com>
2023-05-04 17:15:32 -0400

    step 4

--- psswrdz-app/src/Passwords.js
+++ psswrdz-app/src/Passwords.js
@@ -16,19 +16,29 @@ function generatePassword() {
 }
 
 function Password() {
-    const [password, setPassword] = useState('p@$$w0rd');
-
-  return (
-  <div>
-    <div>{password}</div>
-    <div>
-      <button onClick={(e) => {
-            const newPassword = generatePassword();
-            setPassword(newPassword);
-      }}>Generate</button>
-    </div>
-  </div>
-  )
-}
-
-export default Password
+    const [password, setPassword] = useState('');
+    const [inputValue, setInputValue] = useState('');
+  
+    const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+    };
+  
+    const handleGenerateClick = () => {
+      setPassword(generatePassword());
+    };
+  
+    return (
+      <div>
+        <input 
+          type="text"
+          onChange={handleInputChange}
+          value={password || inputValue} 
+        />
+        <div>
+          <button onClick={handleGenerateClick}>Generate</button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Password;
